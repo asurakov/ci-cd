f46e994ce5923f89e8f82a2e24454ce63fc22975
asurakov <asurakov@gmail.com>
2022-06-08 14:23:55 +0300

    add counter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+const App = () => {
+  const [state, setState] = useState(0)
+
+  const increment = () => setState(prev => prev + 1)
+
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
       </div>
-    );
-  }
-}
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+      <hr/>
+      <h2>value = {state}</h2>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+};
 
 export default App;
+
+
+
